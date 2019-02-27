e1be37e146eff5ed78a26cf5ce991c383d4de37f
Boban Petrovic <petrovicboban.g@gmail.com>
2019-02-27 17:34:02 +0100

    Filtering by CIDR range feature

--- log-filter.py
+++ log-filter.py
@@ -2,21 +2,26 @@ import argparse
 import sys
 import iptools
 
+
 def validate(ip):
-    return iptools.ipv4.validate_ip(ip)
+    """Check if ip is valid"""
+    return iptools.ipv4.validate_ip(ip) or iptools.ipv4.validate_cidr(ip)
+
 
 def filter(ip):
+    """Filter lines on STDIN by ip"""
     for line in sys.stdin:
-        if line.split()[0] == ip:
+        line_ip = line.split()[0]
+        if iptools.ipv4.validate_ip(line_ip) and line_ip in iptools.IpRange(ip):
             print(line, end='')
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Filter access logs by IP address')
-    parser.add_argument('--ip', dest='ip', help='IP address')
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Filter access logs by IP address or CIDR range')
+    parser.add_argument('--ip', dest='ip', help='IP address or CIDR range')
     args = parser.parse_args()
 
     if not validate(args.ip):
-         sys.exit('IP invalid. Exiting.')
+         sys.exit('IP or CIDR range invalid. Exiting.')
 
     filter(args.ip)
